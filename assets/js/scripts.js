3125bb9ec724a44dd75c940e6e5d498e70bdd081
Nguyễn Long <nguyenlong@MacBook-Air-cua-Nguyen.local>
2024-09-11 17:05:06 +0700

    sign in and sign up

---
+++ assets/js/scripts.js
@@ -0,0 +1,18 @@
+function load(selector, path) {
+    const cached = localStorage.getItem(path);
+    if (cached) {
+        document.querySelector(selector).innerHTML = cached;
+    }
+
+    fetch(path)
+        .then((res) => res.text()) // Corrected this line
+        .then((html) => {
+            if (html !== cached) {
+                document.querySelector(selector).innerHTML = html;
+                localStorage.setItem(path, html);
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching the HTML:", error);
+        });
+}
